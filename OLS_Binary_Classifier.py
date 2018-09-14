09553ffeb133ea596ed848414b2b378f2a09d73a
Colton <colton.kinstley@gmail.com>
2018-09-14 02:17:44 +0100

    Modified plotting. Seperated sample drawing and model training.

--- OLS_Binary_Classifier.py
+++ OLS_Binary_Classifier.py
@@ -16,51 +16,49 @@ from sklearn import linear_model
 class Least_Squares_Binary_Classifier:
     def __init__(self):
         self.BLUE_MEAN = (3, 3)
-        self.ORANGE_MEAN = (6, 6)
+        self.ORANGE_MEAN = (5, 5)
         self.BLUE_TRAIN_SIZE = 15
-        self.BLUE_TEST_SIZE = 15
+        self.BLUE_SIZE = 30
         self.ORANGE_TRAIN_SIZE = 15
-        self.ORANGE_TEST_SIZE = 15
+        self.ORANGE_SIZE = 30
 
+        self.generate_new_sample()
         self.train_new_model()
 
+    def generate_new_sample(self):
+        self.blue_set = np.array([
+            list(x) for x in zip(
+                np.random.normal(loc=self.BLUE_MEAN[0], size=self.BLUE_SIZE),
+                np.random.normal(loc=self.BLUE_MEAN[1], size=self.BLUE_SIZE),
+            )
+        ])
+
+        self.orange_set = np.array([
+            list(x) for x in zip(
+                np.random.normal(
+                    loc=self.ORANGE_MEAN[0], size=self.ORANGE_SIZE),
+                np.random.normal(
+                    loc=self.ORANGE_MEAN[1], size=self.ORANGE_SIZE),
+            )
+        ])
+
     def train_new_model(self):
-        self.train_blue = np.array([
-            list(x) for x in zip(
-                np.random.normal(
-                    loc=self.BLUE_MEAN[0], size=self.BLUE_TRAIN_SIZE),
-                np.random.normal(
-                    loc=self.BLUE_MEAN[1], size=self.BLUE_TRAIN_SIZE),
-            )
-        ])
-        self.train_orange = np.array([
-            list(x) for x in zip(
-                np.random.normal(
-                    loc=self.ORANGE_MEAN[0], size=self.ORANGE_TRAIN_SIZE),
-                np.random.normal(
-                    loc=self.ORANGE_MEAN[1], size=self.ORANGE_TRAIN_SIZE),
-            )
-        ])
-        self.test_blue = np.array([
-            list(x) for x in zip(
-                np.random.normal(
-                    loc=self.BLUE_MEAN[0], size=self.BLUE_TEST_SIZE),
-                np.random.normal(
-                    loc=self.BLUE_MEAN[1], size=self.BLUE_TEST_SIZE),
-            )
-        ])
-        self.test_orange = np.array([
-            list(x) for x in zip(
-                np.random.normal(
-                    loc=self.ORANGE_MEAN[0], size=self.ORANGE_TEST_SIZE),
-                np.random.normal(
-                    loc=self.ORANGE_MEAN[1], size=self.ORANGE_TEST_SIZE),
-            )
-        ])
+
+        np.random.shuffle(self.blue_set)  # put the points in a random order
+
+        # take the first part of the shuffled list for traning and last part for test
+        self.blue_training_set = self.blue_set[0:self.BLUE_TRAIN_SIZE]
+        self.blue_test_set = self.blue_set[self.BLUE_TRAIN_SIZE + 1:]
+
+        np.random.shuffle(self.orange_set)  # put the points in a random order
+
+        # take the first part of the shuffled list for traning and last part for test
+        self.orange_training_set = self.orange_set[0:self.ORANGE_TRAIN_SIZE]
+        self.orange_test_set = self.orange_set[self.ORANGE_TRAIN_SIZE + 1:]
 
         self.model = linear_model.LinearRegression()
         self.model.fit(
-            np.concatenate((self.train_blue, self.train_orange)),
+            np.concatenate((self.blue_training_set, self.orange_training_set)),
             np.concatenate((np.zeros(self.BLUE_TRAIN_SIZE),
                             np.ones(self.ORANGE_TRAIN_SIZE))),
         )
@@ -70,43 +68,63 @@ class Least_Squares_Binary_Classifier:
         self.x1 = (.5 - self.model.intercept_) / self.model.coef_[1]
         self.x2 = (.5 - self.model.intercept_) / self.model.coef_[0]
 
-    def plot_results(self):
-        fig1 = plt.figure()
-        plt.scatter(
-            x=self.train_blue[:, 0], y=self.train_blue[:, 1], color="blue")
-        plt.scatter(
-            x=self.train_orange[:, 0],
-            y=self.train_orange[:, 1],
+    def plot_results(self, axis=None):
+        # if an axis object is given then add the plots to it and return it otherwise
+        # create a figure and return that
+        if not axis:
+            fig1 = plt.figure()
+            ax = fig1.add_subplot(111)
+        else:
+            fig1 = None
+            ax = axis
+
+        ax.scatter(
+            x=self.blue_training_set[:, 0],
+            y=self.blue_training_set[:, 1],
+            color="blue")
+        ax.scatter(
+            x=self.orange_training_set[:, 0],
+            y=self.orange_training_set[:, 1],
             color="orange")
-        plt.scatter(
-            x=self.test_blue[:, 0],
-            y=self.test_blue[:, 1],
+        ax.scatter(
+            x=self.blue_test_set[:, 0],
+            y=self.blue_test_set[:, 1],
             color="blue",
             marker="+")
-        plt.scatter(
-            x=self.test_orange[:, 0],
-            y=self.test_orange[:, 1],
+        ax.scatter(
+            x=self.orange_test_set[:, 0],
+            y=self.orange_test_set[:, 1],
             color="orange",
             marker="+",
         )
-        plt.plot([self.x1, 0], [0, self.x2],
-                 color="purple")  # plot intercepts with line connecting
-        fig1.show()
+        ax.plot([self.x1, 0], [0, self.x2],
+                color="purple")  # plot intercepts with line connecting
+
+        ax.set_xlim((0, 10))
+        ax.set_ylim((0, 10))
+
+        if fig1:
+            return fig1
+        else:
+            return ax
 
     def get_errors(self):
         self.errors = []
         for index, prediction in enumerate(
                 self.model.predict(
-                    np.concatenate((self.train_blue, self.test_blue)))):
+                    np.concatenate((self.blue_training_set,
+                                    self.blue_test_set)))):
             if prediction > .5:
                 self.errors.append(
-                    np.concatenate((self.train_blue, self.test_blue))[index])
+                    np.concatenate((self.blue_training_set,
+                                    self.blue_test_set))[index])
 
         for index, prediction in enumerate(
                 self.model.predict(
-                    np.concatenate((self.train_orange, self.test_orange)))):
+                    np.concatenate((self.orange_training_set,
+                                    self.orange_test_set)))):
             if prediction < .5:
                 self.errors.append(
-                    np.concatenate((self.train_orange,
-                                    self.test_orange))[index])
+                    np.concatenate((self.orange_training_set,
+                                    self.orange_test_set))[index])
         return self.errors
